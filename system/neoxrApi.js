157fc63c923c8198d13136599ad39b72fe6041f4
pumpa0 <87925130+pumpa0@users.noreply.github.com>
2023-01-15 19:16:52 +0700

    Update neoxrApi.js

--- system/neoxrApi.js
+++ system/neoxrApi.js
@@ -1,6 +1,6 @@
 module.exports = class NeoxrApi {
    baseUrl = 'https://api.neoxr.my.id/api'
-   apiKey = 'botzhanapi'
+   apiKey = null
 
    constructor(apiKey) {
       this.apiKey = apiKey || ''
